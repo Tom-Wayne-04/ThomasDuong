86a100ec16826e298cb427ed47838d797583877c
TOM <benbinh04100@gmail>
2024-02-29 00:41:54 +0700

    Third Deploy

--- pages/about/test.js
+++ pages/about/test.js
@@ -132,7 +132,7 @@ const About = () => {
                         animate='show'
                         exit='hidden'
                         className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
-                        10 years ago, I began freelancing as a developer. Since then, I've done remote work for agencies
+                        10 years ago, I began freelancing as a developer. Since then, I have done remote work for agencies
                         , consulted for startups, and collaborated on digital products for business and consumer use.
                     </motion.p>
                     {/* counters */}
